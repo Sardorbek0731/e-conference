624a7a712afbca5589752f33fc358ec7171d31d2
Sardorbek Olimov <sardorbek.programming@gmail.com>
2025-03-07 17:17:57 +0500

    added social networks

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,12 @@ import "./Home.css";
 // React Router DOM
 import { NavLink } from "react-router-dom";
 
+// Icons
+import telegram from "../../assets/icons/telegram.png";
+import twitter from "../../assets/icons/twitter.png";
+import instagram from "../../assets/icons/instagram.png";
+import facebook from "../../assets/icons/facebook.png";
+
 function Home() {
   return (
     <section className="home container">
@@ -17,6 +23,21 @@ function Home() {
         </p>
         <NavLink to="#" className="detailBtn">
           Batafsil
+        </NavLink>
+      </div>
+
+      <div className="socialNetworks">
+        <NavLink to="#">
+          <img src={telegram} alt="Telegram Icon" />
+        </NavLink>
+        <NavLink to="#">
+          <img src={twitter} alt="Twitter Icon" />
+        </NavLink>
+        <NavLink to="#">
+          <img src={instagram} alt="Instagram Icon" />
+        </NavLink>
+        <NavLink to="#">
+          <img src={facebook} alt="Facebook Icon" />
         </NavLink>
       </div>
     </section>
