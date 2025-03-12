51285694059e5237f32e46010f27732d9d3ed847
Sardorbek Olimov <sardorbek.programming@gmail.com>
2025-03-12 19:48:41 +0500

    added home image

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,21 +10,26 @@ import twitter from "../../assets/icons/twitter.png";
 import instagram from "../../assets/icons/instagram.png";
 import facebook from "../../assets/icons/facebook.png";
 
+// Images
+import paperPen from "../../assets/images/paper-pen.png";
+
 function Home() {
   return (
     <section className="home container">
       <div className="homeTitle">
         <h1>E-Conference</h1>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic
-          cupiditate qui recusandae. Illo placeat minus ut modi omnis
-          reiciendis, officia atque illum, ipsum velit sequi odit distinctio
-          nemo.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          cum similique perspiciatis eum architecto corrupti eius quas molestias
+          perferendis? Enim repudiandae saepe maxime, iste sunt quidem
+          laudantium dolore recusandae beatae.
         </p>
         <NavLink to="#" className="detailBtn">
           Batafsil
         </NavLink>
       </div>
+
+      <img className="penAndPaper" src={paperPen} alt="Paper And Pen Image" />
 
       <div className="socialNetworks">
         <NavLink to="#">
