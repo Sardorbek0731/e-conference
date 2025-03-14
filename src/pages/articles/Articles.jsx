ab935ce60420ed03c8a32e3b060ad1fced144019
Sardorbek Olimov <sardorbek.programming@gmail.com>
2025-03-14 23:03:50 +0500

    repaired section title

--- src/pages/articles/Articles.jsx
+++ src/pages/articles/Articles.jsx
@@ -7,6 +7,14 @@ import { NavLink } from "react-router-dom";
 function Articles() {
   return (
     <section className="articles container">
+      <div className="sectionTitle">
+        <span>Articles - Статьи</span>
+        <h1>Maqolalar</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          molestias nulla odit esse.
+        </p>
+      </div>
       <NavLink to="/articles/1">Articles</NavLink>
     </section>
   );
