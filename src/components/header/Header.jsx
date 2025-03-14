41e30626b550bcf57fc660f225c52b5aab101717
Sardorbek Olimov <sardorbek.programming@gmail.com>
2025-03-14 22:43:20 +0500

    changed header design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -20,26 +20,21 @@ function Header() {
   }, [location]);
 
   return (
-    <header
-      className={
-        location.pathname.length > 1 ? "container otherPage" : "container"
-      }
-    >
+    <header className={location.pathname.length > 1 ? "otherPage" : ""}>
       <NavLink to="/" className="logo">
         <img src={logo} alt="Logo Icon" />
       </NavLink>
 
       <nav>
         {header.map((item, id) => {
-          return (
+          return location.pathname.length > 1 ? (
             <NavLink to={item.to} key={id}>
-              <img
-                src={
-                  location.pathname.length > 1 ? item.darkIcon : item.lightIcon
-                }
-                alt="Header Icon"
-              />
-              <h1>{item.title}</h1>
+              {item.title}
+            </NavLink>
+          ) : (
+            <NavLink to={item.to} key={id}>
+              <img src={item.icon} alt="Header Icon" />
+              {item.title}
             </NavLink>
           );
         })}
--- src/data/data.js
+++ src/data/data.js
@@ -1,54 +1,40 @@
-// Light Header Icons
-import lightHome from "../assets/icons/header/light/home.png";
-import lightInfo from "../assets/icons/header/light/info.png";
-import lightArticles from "../assets/icons/header/light/articles.png";
-import lightNews from "../assets/icons/header/light/news.png";
-import lightEditorial from "../assets/icons/header/light/editorial.png";
-import lightContact from "../assets/icons/header/light/contact.png";
-
-// Dark Header Icons
-import darkHome from "../assets/icons/header/dark/home.png";
-import darkInfo from "../assets/icons/header/dark/info.png";
-import darkArticles from "../assets/icons/header/dark/articles.png";
-import darkNews from "../assets/icons/header/dark/news.png";
-import darkEditorial from "../assets/icons/header/dark/editorial.png";
-import darkContact from "../assets/icons/header/dark/contact.png";
+// Header Icons
+import home from "../assets/icons/header/home.png";
+import info from "../assets/icons/header/info.png";
+import articles from "../assets/icons/header/articles.png";
+import news from "../assets/icons/header/news.png";
+import editorial from "../assets/icons/header/editorial.png";
+import contact from "../assets/icons/header/contact.png";
 
 export const header = [
   {
-    lightIcon: lightHome,
-    darkIcon: darkHome,
+    icon: home,
     title: "Bosh sahifa",
     to: "/",
   },
   {
-    lightIcon: lightInfo,
-    darkIcon: darkInfo,
+    icon: info,
     title: "Ma'lumotlar",
     to: "/",
   },
   {
-    lightIcon: lightArticles,
-    darkIcon: darkArticles,
+    icon: articles,
     title: "Maqolalar",
     to: "articles",
   },
   {
-    lightIcon: lightNews,
-    darkIcon: darkNews,
+    icon: news,
     title: "Yangiliklar",
     to: "/",
   },
   {
-    lightIcon: lightEditorial,
-    darkIcon: darkEditorial,
+    icon: editorial,
     title: "Tahririyat",
     to: "/",
   },
   {
-    lightIcon: lightContact,
-    darkIcon: darkContact,
-    title: "Aloqa",
+    icon: contact,
+    title: "Bog'lanish",
     to: "/",
   },
 ];
