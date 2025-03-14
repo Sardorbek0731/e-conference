46ed2f4a3756befe9fefd8e6ad1dbf809169a0f0
Sardorbek Olimov <sardorbek.programming@gmail.com>
2025-03-15 00:28:25 +0500

    repaired article card

--- src/pages/articles/Articles.jsx
+++ src/pages/articles/Articles.jsx
@@ -4,9 +4,15 @@ import "./Articles.css";
 // React Router DOM
 import { NavLink } from "react-router-dom";
 
+// Images
+import image from "../../assets/logo/logo.png";
+
+// Icons
+import download from "../../assets/icons/articles/download.png";
+
 function Articles() {
   return (
-    <section className="articles container">
+    <section className="articles">
       <div className="sectionTitle">
         <span>Articles - Статьи</span>
         <h1>Maqolalar</h1>
@@ -15,7 +21,47 @@ function Articles() {
           molestias nulla odit esse.
         </p>
       </div>
-      <NavLink to="/articles/1">Articles</NavLink>
+      <div className="articleCards">
+        <NavLink className="articleCard" to="/articles/1">
+          <div className="articleHeader">
+            <img src={image} alt="Article Image" />
+            <h1>
+              Ftiziatriya fanini o‘qitishda tibbiyot talabalari klinik
+              fikrlashini oshirishda FILA jadvalining ahamiyati
+            </h1>
+          </div>
+          <div className="articleBody">
+            <h3>
+              <span>Muallif:</span> Matkurbonov Khamdambek Ilxambekovich
+            </h3>
+
+            <h5>01.01.2025, 00:00</h5>
+
+            <span className="downloadPDF">
+              <img src={download} alt="Download Icon" />
+              PDF
+            </span>
+          </div>
+        </NavLink>
+        <NavLink className="articleCard" to="/articles/2">
+          <div className="articleHeader">
+            <img src={image} alt="Article Image" />
+            <h1>THE RELEVANCE OF MEASLES TODAY</h1>
+          </div>
+          <div className="articleBody">
+            <h3>
+              <span>Muallif:</span> Sadullaev Siroj Ernazarovich
+            </h3>
+
+            <h5>15.03.2025, 12:05</h5>
+
+            <span className="downloadPDF">
+              <img src={download} alt="Download Icon" />
+              PDF
+            </span>
+          </div>
+        </NavLink>
+      </div>
     </section>
   );
 }
