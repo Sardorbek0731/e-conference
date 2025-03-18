1e9ff757fa64b7f113f272b94b5908123ce34e5e
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2025-03-18 23:07:32 +0500

    removed react-helmet

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,34 +1,56 @@
 // CSS
 import "./Article.css";
+import {useEffect} from "react";
 
 // Data
 
 function Article({ data }) {
   const articleID = JSON.parse(localStorage.getItem("articleID"));
 
-  return (
+    const articleData = {
+        "@context": "https://schema.org",
+        "@type": "Article",
+        "headline": data[0].title,
+        "author": {
+            "@type": "Person",
+            "name": data[0].author
+        },
+        "datePublished": "2025-03-06",
+        "dateModified": "2025-03-06",
+        "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "https://online-article.netlify.app/articles/IWZohXfB6DclUZmDQm6J"
+        },
+        "publisher": {
+            "@type": "Organization",
+            "name": "Sizning sayt nomingiz",
+            "logo": {
+                "@type": "ImageObject",
+                "url": "https://online-article.netlify.app/logo.png"
+            }
+        }
+    };
+
+    useEffect(() => {
+        document.title = data[0].title;
+        const metaDescription = document.createElement("meta");
+        const script = document.createElement("script");
+        metaDescription.name = "description";
+        metaDescription.content = data[0].title;
+        script.type = "application/ld+json";
+        script.textContent = JSON.stringify(articleData);
+        document.head.appendChild(metaDescription);
+        document.head.appendChild(script);
+
+        return () => {
+            document.head.removeChild(metaDescription);
+            document.head.removeChild(script);
+        };
+    }, []);
+
+    return (
       <>
-        <div>
-            <title>{data[0].title}</title>
-            <meta name="description" content={data[0].title} />
-            <script type="application/ld+json">
-              {JSON.stringify({
-                "@context": "http://schema.org",
-                "@type": "ScholarlyArticle",
-                "name": data[0].title,
-                "author": {
-                  "@type": "Person",
-                  "name": data[0].author
-                },
-                "datePublished": data[0].createdAt,
-                "publisher": {
-                  "@type": "Organization",
-                  "name": "Sizning Vebsaytingiz"
-                },
-                "url": window.location.href
-              })}
-            </script>
-        </div>
+        <title>{data[0].title}</title>
         <section className="article container">
           {data.map((item, id) => {
             return (
