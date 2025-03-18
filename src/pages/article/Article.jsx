054706cbc911f136e3fa8cf7a6c49eccae8824d7
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2025-03-18 17:21:09 +0500

    added react helmet

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,5 +1,6 @@
 // CSS
 import "./Article.css";
+import { Helmet } from "react-helmet-async";
 
 // Data
 
@@ -7,18 +8,43 @@ function Article({ data }) {
   const articleID = JSON.parse(localStorage.getItem("articleID"));
 
   return (
-    <section className="article container">
-      {data.map((item, id) => {
-        return (
-          articleID === item.id && (
-            <div className="articleItem" key={id}>
-              <h1>{item.title}</h1>
-              <p>{item.text}</p>
-            </div>
-          )
-        );
-      })}
-    </section>
+      <>
+        <div>
+          <Helmet>
+            <title>{data[0].title}</title>
+            <meta name="description" content={data[0].title} />
+            <script type="application/ld+json">
+              {JSON.stringify({
+                "@context": "http://schema.org",
+                "@type": "ScholarlyArticle",
+                "name": data[0].title,
+                "author": {
+                  "@type": "Person",
+                  "name": data[0].author
+                },
+                "datePublished": data[0].createdAt,
+                "publisher": {
+                  "@type": "Organization",
+                  "name": "Sizning Vebsaytingiz"
+                },
+                "url": window.location.href
+              })}
+            </script>
+          </Helmet>
+        </div>
+        <section className="article container">
+          {data.map((item, id) => {
+            return (
+                articleID === item.id && (
+                    <div className="articleItem" key={id}>
+                      <h1>{item.title}</h1>
+                      <p>{item.text}</p>
+                    </div>
+                )
+            );
+          })}
+        </section>
+      </>
   );
 }
 
