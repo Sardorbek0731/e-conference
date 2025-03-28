2965d1414987fca7144973e488de1741beaa979f
Sardorbek Olimov <sardorbek.programming@gmail.com>
2025-03-28 22:46:05 +0500

    deleted jspdf and html2canvas

--- src/pages/articles/Articles.jsx
+++ src/pages/articles/Articles.jsx
@@ -4,11 +4,8 @@ import BackButton from "../../components/backButton/BackButton";
 import Loading from "../../components/loading/Loading.jsx";
 import { NavLink } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch.jsx";
-import { downloadPDF } from "../../services/articleService.js";
-import { useRef } from "react";
 
 function Articles() {
-  const articleRef = useRef();
   const { data, isPending, error } = useFetch();
 
   if (error) return <p>Xatolik: {error}</p>;
@@ -27,46 +24,30 @@ function Articles() {
         <Loading isPending={isPending} />
       ) : (
         <div className="articleCards">
-          {data.map((article) => {
+          {data.map(({ title, author, addedTime, id }) => {
             return (
-              <div className="articleCard" key={article.id}>
-                <NavLink to={`/articles/${article.id}`}>
+              <div className="articleCard" key={id}>
+                <NavLink to={`/articles/${id}`}>
                   <div className="articleHeader">
                     <img src={image} alt="Article Image" />
-                    <h3>{article.title}</h3>
+                    <h3>{title}</h3>
                   </div>
                   <div className="articleBody">
                     <h4>
-                      <span>Muallif:</span> {article.author}
+                      <span>Muallif:</span> {author}
                     </h4>
                   </div>
                 </NavLink>
 
                 <div className="articleFooter">
                   <div className="articleDownload-buttons">
-                    <span
-                      className="downloadPDF"
-                      onClick={() => {
-                        downloadPDF(article, articleRef);
-                      }}
-                    >
-                      Download (PDF)
-                    </span>
+                    <span className="downloadPDF">Download (PDF)</span>
                     <NavLink to="/" className="zenodo">
                       ZENODO
                     </NavLink>
                   </div>
 
-                  <h5>{article.addedTime}</h5>
-                </div>
-
-                <div className="articlePDF-UI" ref={articleRef}>
-                  <h1 className="articlePDF-UITitle">{article.title}</h1>
-                  <h3 className="articlePDF-UIAuther">{article.author}</h3>
-                  <div
-                    className="articlePDF-UIContent"
-                    dangerouslySetInnerHTML={{ __html: article.content }}
-                  ></div>
+                  <h5>{addedTime}</h5>
                 </div>
               </div>
             );
--- src/services/articleService.js
+++ src/services/articleService.js
@@ -5,8 +5,6 @@ import { addDoc } from "firebase/firestore";
 import { getDocs } from "firebase/firestore";
 import { getDoc } from "firebase/firestore";
 import { deleteDoc } from "firebase/firestore";
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
 
 export const addArticle = async (article) => {
   const docRef = await addDoc(collection(db, "articles"), article);
@@ -39,40 +37,4 @@ export const updateArticle = async (id, updatedData) => {
 
 export const deleteArticle = async (id) => {
   await deleteDoc(doc(db, "articles", id));
-};
-
-export const downloadPDF = async (article, articleRef) => {
-  try {
-    if (!articleRef.current) {
-      throw new Error("Article reference is not available");
-    }
-
-    const element = articleRef.current;
-
-    const canvas = await html2canvas(element);
-    const imgData = canvas.toDataURL("image/png");
-
-    const pdf = new jsPDF("p", "mm", "a4");
-    const imgWidth = 190;
-    const pageHeight = 297;
-    const imgHeight = (canvas.height * imgWidth) / canvas.width;
-
-    let heightLeft = imgHeight;
-    let position = 10;
-
-    pdf.addImage(imgData, "PNG", 10, position, imgWidth, imgHeight);
-    heightLeft -= pageHeight;
-
-    while (heightLeft > 0) {
-      position = heightLeft - imgHeight;
-      pdf.addPage();
-      pdf.addImage(imgData, "PNG", 10, position, imgWidth, imgHeight);
-      heightLeft -= pageHeight;
-    }
-
-    pdf.save(`${article.title}.pdf`);
-  } catch (error) {
-    console.error("Error generating PDF:", error);
-    alert("Failed to generate PDF. Please try again.");
-  }
 };
